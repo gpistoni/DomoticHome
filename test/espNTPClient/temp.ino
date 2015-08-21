79a44100e9089223192d2c30ef2ade645c309e68
giuliano <io@io.it>
2015-08-21 22:19:47 +0200

    *-

--- test/espNTPClient/temp.ino
+++ test/espNTPClient/temp.ino
@@ -8,18 +8,18 @@ class DHwifi
 {
   private:
 
-    WiFiUDP udp;        			// A UDP instance to let us send and receive packets over UDP
-    static const int NTP_PACKET_SIZE = 48;  	// NTP time stamp is in the first 48 bytes of the message
-    byte packetBuffer[ NTP_PACKET_SIZE];  	// buffer to hold incoming and outgoing packets
+    WiFiUDP udp;        // A UDP instance to let us send and receive packets over UDP
+    static const int NTP_PACKET_SIZE = 48;  // NTP time stamp is in the first 48 bytes of the message
+    byte packetBuffer[ NTP_PACKET_SIZE];  // buffer to hold incoming and outgoing packets
 
   public:
     DHwifi() {};
 
     void setup()
     {
-      char ssid[] = "PistoniHomeT";     	// your network SSID (name)
-      char pass[] = "giaco.iren.dario";       	// your network password
-      unsigned int localPort = 2390;      	// local port to listen for UDP packets
+      char ssid[] = "PistoniHomeT";     //  your network SSID (name)
+      char pass[] = "giaco.iren.dario";       // your network password
+      unsigned int localPort = 2390;      // local port to listen for UDP packets
 
       // We start by connecting to a WiFi network
       Serial.print("Connecting to ");
