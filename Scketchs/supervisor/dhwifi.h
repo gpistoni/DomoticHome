0ac1bc9eb2c3032b18cbc58db8de8772c671b319
giuliano <io@io.it>
2015-09-07 19:21:17 +0200

    ww

---
+++ Scketchs/supervisor/dhwifi.h
@@ -0,0 +1,30 @@
+#ifndef dhwifi_h
+#define dhwifi_h
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+class DHwifi
+{
+private:
+
+    WiFiUDP udp;        			// A UDP instance to let us send and receive packets over UDP
+    static const int NTP_PACKET_SIZE = 48;  	// NTP time stamp is in the first 48 bytes of the message
+    byte packetBuffer[ NTP_PACKET_SIZE];  	// buffer to hold incoming and outgoing packets
+
+   String m_host;
+
+public:
+    DHwifi() {};
+
+    void setup( IPAddress ip, IPAddress gateway, IPAddress subnet, String remoteServer = "192.168.1.200" );
+    String HttpRequest( String req = "@get(0,0)" );
+    time_t GetSystemTime();
+
+private:
+    // send an NTP request to the time server at the given address
+    unsigned long sendNTPpacket(IPAddress& address);
+
+};
+
+#endif
---
+++ libraries/DHProtocol/DHProtocol.ino
@@ -0,0 +1,132 @@
+#include <dhprotocol.h>
+
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+#include "webserver.h"
+
+// terminali
+DHProtocol T[8];
+
+SoftwareSerial mySerial(8, 9, 1);  //RX, TX, inverse logic (signal=5v)
+
+// MAC address for the ethernet controller.  -----------------------------
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE };
+
+// IP address for the controller:  -----------------------------
+IPAddress ip(192, 168, 1, 200);                         //<<-- IP
+//IPAddress gateway(172, 31, 8, 1);                     //<<-- GATEWAY
+//IPAddress subnet(255, 255, 255, 0);                   //<<-- SUBNET
+
+// Pins 10, 11, 12 and 13 are reserved for interfacing with the Ethernet module and should not be used otherwise
+// Pin 10 is reserved for the Wiznet interface, SS for the SD card is on Pin 4.
+// There is a built-in LED connected to digital pin 9.
+EthernetServer server(80);
+
+#define ETHERNET_SELECT 10
+
+void setup()
+{
+  T[1].setup(0, 1, &mySerial );  // -
+  T[2].setup(0, 2, &mySerial );  // --
+  T[3].setup(0, 3, &mySerial );  // rele' pdc
+  T[4].setup(0, 4, &mySerial );  // temp caldaie
+  T[5].setup(0, 5, &mySerial );  // rele pavimento
+  T[6].setup(0, 6, &mySerial );  // --
+  T[7].setup(0, 7, &mySerial );  // --
+
+  Serial.begin(9600);
+  OUTLN("System Start");
+
+  //inifile ************************************************************
+  pinMode(ETHERNET_SELECT, OUTPUT);
+  digitalWrite(ETHERNET_SELECT, HIGH); // disable Ethernet
+
+  //ethernet ************************************************************
+  Ethernet.begin(mac, ip);
+  server.begin();
+
+  // give the sensor and Ethernet shield time to set up:
+  delay(1000);
+
+  OUTLN("server is at ");
+  OUTLN(Ethernet.localIP());
+  
+  pinMode(8, INPUT);
+  pinMode(9, OUTPUT);
+}
+
+int count = 0;
+unsigned int lastDataLogging = 0;
+
+void loop()
+{
+  listenForEthernetClients();
+  count++;
+  if ( T[1].checkTiming(2000) )
+  {
+    T[1].sendRequest();
+    T[1].waitData( 100 );
+  };
+
+  listenForEthernetClients();
+
+  if ( T[2].checkTiming(2000) )
+  {
+    T[2].sendRequest();
+    T[2].waitData( 100 );
+  };
+
+  listenForEthernetClients();
+
+  if ( T[3].checkTiming(2000) )
+  {
+    T[3].sendRequest();
+    T[3].waitData( 100 );
+  };
+
+  listenForEthernetClients();
+
+  if ( T[4].checkTiming(2000) )
+  {
+    T[4].sendRequest();
+    T[4].waitData( 100 );
+  };
+
+  listenForEthernetClients();
+
+  if ( T[5].checkTiming(10000) )
+  {
+    T[5].sendRequest();
+    T[5].waitData( 100 );
+  };
+
+  listenForEthernetClients();
+
+  if ( T[6].checkTiming(10000) )
+  {
+    T[6].sendRequest();
+    T[6].waitData( 100 );
+  };
+
+  listenForEthernetClients();
+
+  if ( T[7].checkTiming(10000) )
+  {
+    T[7].sendRequest();
+    T[7].waitData( 100 );
+  };
+
+  listenForEthernetClients();
+/*
+  OUT(".");
+  OUT( millis() );
+  OUT( " freeMemory()=" );
+  OUTLN( freeMemory() );
+  delay(200);
+*/
+}
+
+
+
