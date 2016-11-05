f97b6310873c65c40e6362a4289de5abcc442eb0
gpisto <gpisto>
2016-11-05 19:02:52 +0100

    light

--- Scketchs/Terminal4/Terminal4.ino
+++ Scketchs/Terminal4/Terminal4.ino
@@ -1,13 +1,8 @@
 // MODULO 04
-<<<<<<< HEAD
-// luci esterne
-// rev: 10 nov 2016
-=======
 // sonde temperatura locale caldaia
 // ingressi sonde 2-9 onewire con resisteza pullup
 // send: tempertaura in sensor [0..8] 
 // rev: 16 ago 2015
->>>>>>> 7449b2f55019382b15e27aa5b815d339d05e84a4
 
 #include <cdht.h>
 #include <dhprotocol.h>
@@ -26,30 +21,18 @@ SoftwareSerial mySerial(10, 12, TRUE);          // RX 10, TX 12 Serial, inverse 
 
 void setup()
 {
-<<<<<<< HEAD
-  Slave.setup(4, 0, &mySerial);     // Terminal ID
-
-  pinMode(2, OUTPUT);     // 2-5 relay.
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  
-=======
   Slave.setup(4, 0, &mySerial);     		// Terminal ID
   Probes.setup(2,3,4,5,6,7,8,9);        	// 4 temp
  
   Serial.begin(9600);                     	// disabilito le seriali 
->>>>>>> 7449b2f55019382b15e27aa5b815d339d05e84a4
   Serial.print( "Setup-- SLAVE ID: " );
   Serial.print( Slave.m_id );
-
 }
 
 unsigned long old_Read = 0;
 
 void loop()
 {
-   
   if ( Slave.waitRequest( 50 ) )
   {
      Slave.sendData();
