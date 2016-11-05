7449b2f55019382b15e27aa5b815d339d05e84a4
io@io.ru <io@io.ru>
2016-11-05 17:48:24 +0100

    r Changes to be committed: r

--- Scketchs/Terminal4/Terminal4.ino
+++ Scketchs/Terminal4/Terminal4.ino
@@ -1,49 +1,63 @@
-// MODULO 05
+// MODULO 04
 // sonde temperatura locale caldaia
-// rev: 10 nov 2016
+// ingressi sonde 2-9 onewire con resisteza pullup
+// send: tempertaura in sensor [0..8] 
+// rev: 16 ago 2015
 
+#include <cdht.h>
 #include <dhprotocol.h>
+#include <cprobe.h>
 
+#include <DHT.h>
 #include <OneWire.h>
 #include <SoftwareSerial.h>
 
 
+CProbe  Probes;
+
 DHProtocol Slave;
-SoftwareSerial mySerial(11, 12, TRUE);     // 11,12 Serial
+SoftwareSerial mySerial(10, 12, TRUE);          // RX 10, TX 12 Serial, inverse logic
 
 
 void setup()
 {
-  Slave.setup(4, 0, &mySerial);     // Terminal ID
-
-  pinMode(2, OUTPUT);     // 2-10 relay.
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  
+  Slave.setup(4, 0, &mySerial);     		// Terminal ID
+  Probes.setup(2,3,4,5,6,7,8,9);        	// 4 temp
+ 
+  Serial.begin(9600);                     	// disabilito le seriali 
   Serial.print( "Setup-- SLAVE ID: " );
   Serial.print( Slave.m_id );
+
 }
 
-bool b;
+unsigned long old_Read = 0;
 
 void loop()
-{  
-  /*******************************************************************************/
-  if ( Slave.waitRequest(50) )
+{
+   
+  if ( Slave.waitRequest( 50 ) )
   {
-    Slave.relay[0] = !Slave.relay[0];
-     
-    Serial.print( "\nSet Relay: " );
+     Slave.sendData();
+     return;
+  }
+  
+/*******************************************************************************/
+  unsigned long now = millis();               // Terminal ID
+  if ( now - old_Read >= 10000)  //leggo probe ogni 2 secondi
+  {
+    old_Read = now;
 
-    for (int i = 0; i < 4; i++)
+    Probes.readvalues();
+
+    for ( int i = 0; i < Probes.nums; i++)
     {
-      Slave.sensor[i] = Slave.relay[i];
-      digitalWrite(2 + i, !Slave.relay[i] );
-      if (i != 0)  Serial.print( "," );
-      Serial.print( Slave.relay[i] );
-    }
-    Slave.sendData();
-  } 
+      Slave.sensor[i] = Probes.t[i] * 10;
+    
+      Serial.println( "");
+      Serial.print( "Probe:" );
+      Serial.print( Slave.sensor[i] );
+     }
+  }
+  /*****************************************************************************/
 };
 
