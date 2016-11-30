c81b4b2416f471e69403dc230402518f4d7c8017
Your Name <you@example.com>
2016-11-30 21:22:08 +0100

    christmas

--- Projects/FadingChristmas/FadingChristmas.ino
+++ Projects/FadingChristmas/FadingChristmas.ino
@@ -5,31 +5,27 @@ void setup()
   // nothing happens in setup
 }
 
-int nc = 0;
-
 void loop()
 {
-  nc++;
-  nc = nc % 3;
-
-  for (int fadeValue = 3 ; fadeValue <= 128; fadeValue += 1 )
+  for (int fadeValue=0 ; fadeValue<=12; fadeValue+=1 )
   {
-    analogWrite(ledPin, fadeValue);
-    delay(30);
-  }
-
-  for (int c = 0 ; c <= nc; c++ )
-  {
-    analogWrite(ledPin, 255);
-    delay(100);
-    analogWrite(ledPin, 64);
+    analogWrite(ledPin, fadeValue*fadeValue);
     delay(50);
   }
 
-  for (int fadeValue = 128 ; fadeValue >= 3; fadeValue -= 1 )
+  for (int c = 0 ; c <= 3; c++ )
+  {
+    analogWrite(ledPin, 200);
+    delay(100);
+    analogWrite(ledPin, 64);
+    delay(100);
+  }
+   analogWrite(ledPin, 200);
+   
+  for (int fadeValue=120 ; fadeValue>=0; fadeValue-=10 )
   {
     analogWrite(ledPin, fadeValue);
-    delay(30);
+    delay(50);
   }
 }
 
