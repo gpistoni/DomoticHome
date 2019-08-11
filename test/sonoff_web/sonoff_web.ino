634f13fee43a89d879672e4aaba91a6f6f0a5d45
gpisto <pistoni.giuliano@gmail.com>
2019-08-11 12:02:28 +0200

    sonoff

--- test/sonoff_web/sonoff_web.ino
+++ test/sonoff_web/sonoff_web.ino
@@ -6,14 +6,18 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <ESP8266WiFiMulti.h>
 
 MDNSResponder mdns;
+ESP8266WiFiMulti wifiMulti;
+ESP8266WebServer server(80);
+
 
 // Replace with your network credentials
-const char* ssid = "PistoniHome";
-const char* password = "giaco1iren1dario";
-
-ESP8266WebServer server(80);
+const char* ssid_1 = "PistoniHome";
+const char* ssid_2 = "PistoniHome";
+const char* password_1 = "giaco1iren1dario";
+const char* password_2 = "giaco1iren1dario";
 
 String web_on_html = "1 <p><a href=\"on\"><button>ON</button></a>&nbsp;<a href=\"off\"><button>OFF</button></a></p>";
 String web_off_html = "0 <p><a href=\"on\"><button>ON</button></a>&nbsp;<a href=\"off\"><button>OFF</button></a></p>";
@@ -22,24 +26,27 @@ int gpio_13_led = 13;
 int gpio_12_relay = 12;
 
 int stato = 0;
+static unsigned long last_io = 0;
+
+//declare reset function @ address 0
+void(*resetFunc) (void) = 0; 
 
 void setup(void) 
 {
   //  Init
   pinMode(gpio_13_led, OUTPUT);
-  digitalWrite(gpio_13_led, HIGH);
-
   pinMode(gpio_12_relay, OUTPUT);
-  digitalWrite(gpio_12_relay, HIGH);
 
   Serial.begin(115200);
   delay(5000);
 
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting to wifi..");
+  wifiMulti.addAP(ssid_1, password_1);
+  wifiMulti.addAP(ssid_2, password_2);
+    
+  Serial.println("Connecting to multiple wifi..");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED)
+  while (wifiMulti.run() != WL_CONNECTED)
   {
     digitalWrite(gpio_13_led, LOW);
     delay(500);
@@ -52,33 +59,44 @@ void setup(void)
 
   Serial.println("");
   Serial.print("Connected to ");
-  Serial.println(ssid);
+  Serial.println(WiFi.SSID());
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (mdns.begin("esp8266", WiFi.localIP())) {
+  if (mdns.begin("esp8266", WiFi.localIP())) 
+  {
     Serial.println("MDNS responder started");
   }
 
-  server.on("/", []() {
-    if (digitalRead(gpio_12_relay) == HIGH) {
+  server.on("/", []() 
+  {
+    if (digitalRead(gpio_12_relay) == HIGH)  {
       server.send(200, "text/html", web_on_html);
     } else {
       server.send(200, "text/html", web_off_html);
     }
+        last_io = millis();
   });
 
   server.on("/on", []() 
   {
     server.send(200, "text/html", web_on_html);
     stato = HIGH;
-    delay(100);
+    digitalWrite(gpio_13_led, stato);
+    delay(200);
+    digitalWrite(gpio_13_led, !stato);
+    delay(200);
+        last_io = millis();
   });
 
   server.on("/off", []() {
     server.send(200, "text/html", web_off_html);
     stato = LOW;
-    delay(100);
+    digitalWrite(gpio_13_led, stato);
+    delay(200);
+    digitalWrite(gpio_13_led, !stato);
+    delay(200);
+    last_io = millis();
   });
 
   server.begin();
@@ -87,15 +105,21 @@ void setup(void)
 
 void loop(void)
 {
-
+  
   static unsigned long last = 0;
-  if ( millis() - last > 10 /*sec*/ * 1000 )
+  if ( millis() - last > 30 /*sec*/ * 1000 )  //30 sec
   {
-    digitalWrite(gpio_13_led, relay);
-    delay(50);
-    digitalWrite(gpio_13_led, !relay);
-    delay(50);
+    digitalWrite(gpio_13_led, stato);
+    delay(5);
+    digitalWrite(gpio_13_led, !stato);
+    delay(5);
     last = millis();
+    Serial.println(".");
+  }
+
+  if ( millis() - last_io > 30 * 60 * 1000 ) // 30 minutes
+  {
+    resetFunc();
   }
 
   //client
