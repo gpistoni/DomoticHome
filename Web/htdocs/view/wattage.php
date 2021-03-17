7780e7b53768aa541b1a7fca9063467779e4b6f3
gpisto <pistoni.giuliano@gmail.com>
2021-03-17 21:32:14 +0100

    ok

--- Web/htdocs/view/wattage.php
+++ Web/htdocs/view/wattage.php
@@ -1,3 +1,11 @@
+ <?php 
+use \Phppot\DataQuery;
+use \Phppot\PrintObjects;
+
+require_once "./class/DataQuery.php";
+require_once "./class/PrintObjects.php";
+?>
+		
  <?php require_once (__DIR__ . "/_head.php"); ?>
 
 <body>
@@ -20,89 +28,42 @@
 			  </div>
 			</div>
 	</div>
-	
-<<<<<<< HEAD
-	$dataquery = new DataQuery();
-	$result = $dataquery->Wattage_dayh(4);
 
-	$sz = count($result);
-=======
 	<ul class="list-group">
 		<li>
 		<p>Produzione Oraria</p>
 		
-		<?php
-		use \Phppot\DataQuery;
-		use \Phppot\PrintObjects;
->>>>>>> 62ff707c3d9c6fbe04d8b70f3e9bc9b535b9e884
-
-		require_once "./class/DataQuery.php";
-		require_once "./class/PrintObjects.php";
+		<?php	
 		
 		$dataquery = new DataQuery();
-		$result = $dataquery->Wattage_dayh();
+		$result = $dataquery->Wattage_dayh(1);
 
 		$sz = count($result);
 		
-<<<<<<< HEAD
-		echo "<tr>";
-		echo "<td>" . $row['Day'] . "</td>";
-		echo "<td>" . $row['Produced'] . "</td>";
-		echo "<td>" . $row['Consumed'] . "</td>";
-		echo "<td>" . $row['L1'] . "</td>";
-		echo "<td>" . $row['L2'] . "</td>";
-		echo "<td>" . $row['L3'] . "</td>";
-		echo "</tr>";
-	}
-	echo "</table>";
-	
-	//////////////////////////////////////////////////////////
-	$result = $dataquery->Wattage_all();
-
-	$sz = count($result);
-
-	echo "<table border='1'>
-	<tr>
-	<th>Day</th>
-	<th>Produced</th>
-	<th>Consumed</th>
-	<th>L1</th>
-	<th>L2</th>
-	<th>L3</th>
-	</tr>";
-
-	for($i=0; $i<$sz; $i++)
-	{
-		$row = $result[$i];
-		
-		echo "<tr>";
-		echo "<td>" . $row['Dt'] . "</td>";
-		echo "<td>" . $row['Produced'] . "</td>";
-		echo "<td>" . $row['Consumed'] . "</td>";
-		echo "<td>" . $row['L1'] . "</td>";
-		echo "<td>" . $row['L2'] . "</td>";
-		echo "<td>" . $row['L3'] . "</td>";
-		echo "</tr>";
-	}
-	echo "</table>";
-	
-	//echo "</br>";
-	//DEBUG//
-	//for($i=0; $i<$sz; $i++)
-	//{
-	//	$row = $result[$i];
-	//	print_r($row);
-	//	}	
-	?>
-=======
 		echo PrintObjects::BuildTable( $result, "class='table table-striped table-sm w-auto'");
 		
 		//print_r($result);
 		?>
+		</li>
+		
+		<li>
+		<p>Produzione Media</p>		
+		<?php
+	
+		$dataquery = new DataQuery();
+		$result = $dataquery->Wattage_dayh(8);
+	
+		$sz = count($result);
+		
+		echo PrintObjects::BuildTable( $result, "class='table table-striped table-sm w-auto'");
+		
+		//print_r($result);
+		?>
+		</li>		
+
 		
 		</li>
 	</ul>
->>>>>>> 62ff707c3d9c6fbe04d8b70f3e9bc9b535b9e884
 	
 </body>
 </html>
