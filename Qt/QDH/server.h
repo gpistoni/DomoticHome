6234bc7343f24cd24d5d76766f57ba560ea5ef5b
arm <gpisto@arm.it>
2021-11-28 17:48:50 +0100

    christmas-light

--- Qt/QDH/server.h
+++ Qt/QDH/server.h
@@ -6,7 +6,7 @@
 #include "QTimer"
 #include "dbmanager.h"
 
-#define SERVER_VER "1.11.3"
+#define SERVER_VER "1.11.4"
 
 class ServerDH : public QObject
 {
@@ -35,6 +35,10 @@ private:
     {
         return QDateTime::currentDateTime().date().month();
     }
+    int day()
+    {
+        return QDateTime::currentDateTime().date().day();
+    }
     int hour()
     {
         return QDateTime::currentDateTime().time().hour();
@@ -44,7 +48,6 @@ private:
         return QDateTime::currentDateTime().time().minute();
     }
 
-    QElapsedTimer t_UpdateValues;
     QElapsedTimer t_DbLog;
     QElapsedTimer t_PushWebData;
     QElapsedTimer t_BoilerACS;
@@ -56,6 +59,7 @@ private:
 
     QElapsedTimer t_InternetConnection;
     QElapsedTimer t_Remote212;
+    QElapsedTimer t_Remote216;
 
 public:
     void Stop(){m_running = false; }
@@ -72,8 +76,9 @@ public:
     void manage_PDC(int sec);
     void manage_Pumps(int sec);
 
-    void manage_Internet(int sec);
-    void manage_Remote212(int sec);
+    void manage_Remote210_Internet(int sec);
+    void manage_Remote212_Freezer(int sec);
+    void manage_Remote216_Christmas(int sec);
 
 public slots:
     void run();
